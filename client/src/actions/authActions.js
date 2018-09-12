989613f888c402db50089e79af455a5e62cd21b3
Parvez Nawaz <ParvezN@gs1ca.org>
2018-09-12 17:14:25 -0400

    Login user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERROR } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import { GET_ERROR, SET_CURRENT_USER } from "./types";
 
 // Register User
 export const registerUser = (userData, history) => dispacth => {
@@ -13,3 +15,37 @@ export const registerUser = (userData, history) => dispacth => {
       })
     );
 };
+
+// Login - Get User Token
+export const loginUser = userData => dispacth => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Saveto localStorage
+      const { token } = res.data;
+      // Set token to localStorage
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispacth(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispacth({
+        type: GET_ERROR,
+        payload: err.response.data
+      })
+    );
+};
+
+export const setCurrentUser =  (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
+
+
+// Log user out
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,6 @@
-import { GET_ERROR } from "../actions/types";
+import { SET_CURRENT_USER } from "../actions/types";
+import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from "constants";
+import isEmpty from '../validation/is-empty';
 
 const initialState = {
   isAuthenticated: false,
@@ -7,9 +9,10 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case GET_ERROR:
+    case SET_CURRENT_USER:
       return {
         ...state,
+        isAuthenticated: !isEmpty(action.payload),
         user: action.payload
       };
     default:
