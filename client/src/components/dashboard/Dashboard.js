b8c1da0ff5655c0c66157df5958e1cc532b7357c
Parvez Nawaz <ParvezN@gs1ca.org>
2018-09-19 10:37:18 -0400

    profile in dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {getCurrentProfile} from "../../actions/profileActions";
 import Spinner from '../common/Spinner';
+import {Link} from "react-router-dom";
 
 class Dashboard extends Component {
   componentDidMount(){
@@ -16,7 +17,19 @@ class Dashboard extends Component {
     if(profile === null || loading ){
       dashboardContent = <Spinner/>
     } else {
-      dashboardContent = <h1>Hello</h1>
+      console.log(Object.keys(profile).length);
+      if(Object.keys(profile).length>0){
+        dashboardContent=<h4>Todo</h4>
+      } else{
+        dashboardContent=(
+        <div>
+          <p className="lead text-muted">Welcome {user.name}</p>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-lg btn-info">
+            Create Profile
+          </Link>
+        </div>);
+      }
     }
 
     return (
